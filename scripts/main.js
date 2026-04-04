676c0bf351aad13f5f71c937e41d1564bf5c3821
Seraphalim4404 <nickolosebastian@gmail.com>
2026-04-05 02:10:37 +0800

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+const mod = Vars.mods.locateMod("Lost");
+mod.meta.displayName = "[#8ca4f5]Lost";
+mod.meta.description = "something code borrowed from sapphirium.";
+mod.meta.author = "???";
+
+require("UStatuses");
