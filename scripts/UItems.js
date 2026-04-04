0fac0a9c1dc7a174d5104c74680759c0fe69d58f
Seraphalim4404 <nickolosebastian@gmail.com>
2026-04-05 02:18:11 +0800

    Delete scripts directory

--- scripts/UItems.js
+++
@@ -1,8 +0,0 @@
-const esther = extend(Item, "esther", {
-    cost: 30,
-    flammability: 1
-    color: Color.valueOf("b2c5d1")
-});
-module.exports = {
-    esther: esther
-}
--- scripts/UStatuses.js
+++
@@ -1,5 +0,0 @@
-const forceField = new ForceFieldAbility(60, 0.1, 200, 60 * 5);
-
-module.export = {
-  forceField: ForceField
-}
