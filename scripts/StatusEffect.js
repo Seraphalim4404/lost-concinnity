753e56d71a7a3f74975332fcabb69dac0e30cd29
Seraphalim4404 <nickolosebastian@gmail.com>
2026-05-08 23:43:10 +0800

    Update StatusEffect.js

--- scripts/StatusEffect.js
+++ scripts/StatusEffect.js
@@ -1,9 +1,12 @@
-try {
-  function placeholder(Events.run(Trigger.update) { if(Mathf.chance(2)) { Vars.player.unit().apply(Vars.content.get(lost-restrained), 60)  }};
-    if(!Vars.headless && this.effect !== Fx.none && !unit.inFogTo(Vars.player.team()) && unit.health / unit.maxHealth <= 0.25) {
-    Tmp.v1.rnd(Mathf.range(unit.type.hitSize * 0.5));
-    this.effect.at(unit.x + Tmp.v1.x, unit.y + Tmp.v1.y, 0.0, this.color, this.parentizeEffect ? unit : null);
+Events.run(Trigger.update, setStatus(event));
+  
+function setStatus(event) {
+    if(Mathf.chance(2)) { 
+        Vars.player.unit().apply(Vars.content.get(lost-restrained), 60)
     };
-  } catch(e){
-  Vars.ui.showInfoToast(e + "-StatusEffect", 5);
-  }
+
+    if(!Vars.headless && this.effect !== Fx.none && !unit.inFogTo(Vars.player.team()) && unit.health / unit.maxHealth <= 0.25) {
+        Tmp.v1.rnd(Mathf.range(unit.type.hitSize * 0.5));
+        this.effect.at(unit.x + Tmp.v1.x, unit.y + Tmp.v1.y, 0.0, this.color, this.parentizeEffect ? unit : null);
+  };
+}
